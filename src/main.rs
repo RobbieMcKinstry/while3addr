f672f4784535deae350998f39e9c40a74a64e6de
Robbie McKinstry <rem89@pitt.edu>
2016-05-13 05:55:46 -0400

    got it working!

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 extern crate regex;
 
-use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::File;
 use std::iter::Iterator;
 use std::collections::HashMap;
-use regex::Regex;
 
 enum Op {
     Plus,
@@ -34,44 +32,39 @@ fn main() {
     let mut state  = HashMap::new();
 
     let f = File::open("foo.txt").unwrap();
-    let mut reader = BufReader::new(f);
+    let reader = BufReader::new(f);
 
     let lines: Vec<String> = reader.lines().filter_map(|result| result.ok()).collect();
 
     let mut line: usize = 0;
     let mut has_next: bool = line < lines.len();
     while has_next {
-        let line_str = lines[line];
+        let line_str = lines[line].clone();
 
         println!("{}", line_str);
 
         let split: Vec<&str>  = line_str.split_whitespace().collect();
 
-        let next: I;
+        let next: I =
+            if split[0] == "goto" {
+                let n = split[1].parse::<i32>().unwrap();
+                I::Goto(n)
+            } else if split[0] == "if" {
 
-        if split[0] == "goto" {
-            let n = split[1].parse::<i32>().unwrap();
-            next = I::Goto(n);
-        } else if split[0] == "if" {
+                let x = split[1].to_string();
+                let n = split[3].parse::<i32>().unwrap();
 
-            let x = split[1].to_string();
-            let n = split[3].parse::<i32>().unwrap();
+                // need to parse the opr
+                let opr: Opr = if split[2] == "<" {
+                    Opr::LessThan
+                } else {
+                    Opr::Eq
+                };
 
-            // need to parse the opr
-            let opr: Opr = if split[2] == "<" {
-                Opr::LessThan
-            } else {
-                Opr::Eq
-            };
-
-            next = I::IfSt(x, opr, n);
-       
-        } else if split[1] == "=" {
-            // then this is either constant assigment, copy, or oper
-
-            if split.len() > 3 {
+                I::IfSt(x, opr, n)
+           
+            } else if (split[1] == "=") && (split.len() > 3) { 
                 // then its a op
-                // need to collect the +/-/*//
                 let op: Op = match split[3] {
                     "+" => Op::Plus,
                     "-" => Op::Minus,
@@ -83,26 +76,26 @@ fn main() {
                 let y = split[2].to_string();
                 let z = split[4].to_string();
 
-                next = I::Oper(op, x, y, z);
+                I::Oper(op, x, y, z)
 
-            } else if split[2].parse::<i32>().is_ok() {
+            } else if split[1] == "=" && split[2].parse::<i32>().is_ok() {
                 // then it is a constant
-                next = I::Constant(split[0].to_string(), split[2].parse::<i32>().unwrap())
-            } else {
+                I::Constant(split[0].to_string(), split[2].parse::<i32>().unwrap())
+            } else if split[1] == "=" {
                 // its a copy
-                next = I::CopyVar(split[0].to_string(), split[2].to_string())
-            }
-        } else {
-            println!("Not a valid instruction! Error!")
-        }
+                I::CopyVar(split[0].to_string(), split[2].to_string())
+            } else {
+                println!("Bad instruction!!!");
+                I::Goto(0)
+            };
         // Now, match on the type of oper.
         match next {
             I::Constant(x, val) => {
                 state.insert(x, val);
             },
             I::CopyVar(x, y) => {
-                let val = state.get(&y).unwrap();
-                state.insert(x, *val);
+                let val = *state.get(&y).unwrap();
+                state.insert(x, val);
             },
             I::Oper(op, x, y, z) => {
                 let y_val = *state.get(&y).unwrap();
