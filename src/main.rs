4b0116c70fbf00c5e779fac31f0e90617d095076
Robbie McKinstry <rem89@pitt.edu>
2016-06-29 20:42:56 -0400

    Refactor to break monolithic main into small funcs

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ use std::fs::File;
 use std::iter::Iterator;
 use std::collections::HashMap;
 
+// TODO turn this into a small step semantic model.
+// Pass in the state, and return a configuration (state + line counter)
+// Have this as a trait
+
 enum Op {
     Plus,
     Minus,
@@ -26,10 +30,9 @@ enum I {
     IfSt(String, Opr, i32),
 }
 
-
 fn main() {
 
-    let mut state  = HashMap::new();
+    let mut state = HashMap::new();
 
     let f = File::open("foo.txt").unwrap();
     let reader = BufReader::new(f);
@@ -45,121 +48,141 @@ fn main() {
 
         let split: Vec<&str>  = line_str.split_whitespace().collect();
 
-        let next: I =
-            if split[0] == "goto" {
-                let n = split[1].parse::<i32>().unwrap();
-                I::Goto(n)
-            } else if split[0] == "if" {
-
-                let x = split[1].to_string();
-                let n = split[5].parse::<i32>().unwrap();
-
-                // need to parse the opr
-                let opr: Opr = if split[2] == "<" {
-                    Opr::LessThan
-                } else {
-                    Opr::Eq
-                };
-
-                I::IfSt(x, opr, n)
-           
-            } else if (split[1] == "=") && (split.len() > 3) { 
-                // then its a op
-                let op: Op = match split[3] {
-                    "+" => Op::Plus,
-                    "-" => Op::Minus,
-                    "*" => Op::Times,
-                    "/" => Op::Div,
-                    _   => Op::Plus,
-                };
-                let x = split[0].to_string();
-                let y = split[2].to_string();
-                let z = split[4].to_string();
-
-                I::Oper(op, x, y, z)
-
-            } else if split[1] == "=" && split[2].parse::<i32>().is_ok() {
-                // then it is a constant
-                I::Constant(split[0].to_string(), split[2].parse::<i32>().unwrap())
-            } else if split[1] == "=" {
-                // its a copy
-                I::CopyVar(split[0].to_string(), split[2].to_string())
-            } else {
-                println!("Bad instruction!!!");
-                I::Goto(0)
-            };
+        let next: I = get_instruction(split);
         // Now, match on the type of oper.
         match next {
-            I::Constant(x, val) => {
-                state.insert(x, val);
-            },
+            I::Constant(x, val) => 
+                exec_constant(&mut state, x, val),
             I::CopyVar(x, y) => {
-                let val = *state.get(&y).unwrap();
-                state.insert(x, val);
+                exec_copy(&mut state, x, y)
             },
             I::Oper(op, x, y, z) => {
-                let y_val = *state.get(&y).unwrap();
-                let z_val = *state.get(&z).unwrap();
-
-                let result = match op {
-                    Op::Plus  => y_val + z_val,
-                    Op::Minus => y_val - z_val,
-                    Op::Times => y_val * z_val,
-                    Op::Div   => y_val / z_val,
-                };
-                state.insert(x, result);
+                exec_oper(&mut state, op, x, y, z)
             },
             I::Goto(n) => {
-                let goto_num = n as usize;
-                line = goto_num - 1usize;
+                line = exec_goto(n); // gross
             },
             I::IfSt(x, opr, n) => {
-                let x_val = *state.get(&x).unwrap();
-                let result = match opr {
-                    Opr::LessThan => x_val < 0,
-                    Opr::Eq       => x_val == 0,
-                };
-                if result {
-                    let goto_num = n as usize;
-                    line = goto_num - 1usize;
-                }
+                line = exec_if(&mut state, x, opr, n, line)
             },
         }
+
         line = line + 1usize;
         has_next = line < lines.len();
     }
 
+    println!("\n");
     for (var, val) in &state {
         println!("{}:\t{}", var, val);
     }
 }
-    /*
-    let x: Opr = Opr::LessThan;
-    let y: Op  = Op::Plus;
-    let z: I   = I::Oper(y, 5, 10, 15);
+
+// Determines which instruction there is to run
+fn get_instruction(split: Vec<&str>) -> I {
     
-    match x {
-        Opr::LessThan => println!("Hello Rust!"),
-        Opr::Eq => println!("Rust Sucks!"),
+    return if split[0] == "goto" {
+        // then it must be a goto statement
+        parse_goto(split)
+    } else if split[0] == "if" {
+        // then it must be an if statement
+        parse_if(split)  
+    } else if (split[1] == "=") && (split.len() > 3) { 
+        // then its a op
+        parse_op(split)
+    } else if split[1] == "=" && split[2].parse::<i32>().is_ok() {
+        // then it is a constant
+        parse_constant(split)
+    } else if split[1] == "=" {
+        // its a copy
+        parse_copy(split)
+    } else {
+        // then it is malformed syntax
+        println!("Bad instruction!!!");
+        I::Goto(0)
+    };
+}
+
+fn parse_goto(split: Vec<&str>) -> I {
+    let n = split[1].parse::<i32>().unwrap();
+    I::Goto(n)
+}
+
+fn parse_if(split: Vec<&str>) -> I {
+    let x = split[1].to_string();
+    let n = split[5].parse::<i32>().unwrap();
+
+    // need to parse the opr
+    // since we're not sure what kind of relational operation it is yet
+    let opr: Opr = if split[2] == "<" {
+        Opr::LessThan
+    } else {
+        Opr::Eq
+    };
+
+    I::IfSt(x, opr, n)
+}
+
+fn parse_op(split: Vec<&str>) -> I {
+    let op: Op = match split[3] {
+        "+" => Op::Plus,
+        "-" => Op::Minus,
+        "*" => Op::Times,
+        "/" => Op::Div,
+        _   => Op::Plus,
+    };
+    let x = split[0].to_string();
+    let y = split[2].to_string();
+    let z = split[4].to_string();
+
+    I::Oper(op, x, y, z)
+}
+
+fn parse_constant(split: Vec<&str>) -> I {
+    I::Constant(split[0].to_string(), split[2].parse::<i32>().unwrap())
+}
+
+fn parse_copy(split: Vec<&str>) -> I {
+    I::CopyVar(split[0].to_string(), split[2].to_string())
+}
+
+fn exec_constant(state: &mut HashMap<String, i32>, x: String, val: i32) {
+    state.insert(x, val);
+}
+
+fn exec_copy(state: &mut HashMap<String, i32>, x: String, y: String) {
+    let val = *state.get(&y).unwrap();
+    state.insert(x, val);
+}
+
+fn exec_oper(state: &mut HashMap<String, i32>, op: Op,  x: String, y: String, z: String) {
+    let y_val = *state.get(&y).unwrap();
+    let z_val = *state.get(&z).unwrap();
+
+    let result = match op {
+        Op::Plus  => y_val + z_val,
+        Op::Minus => y_val - z_val,
+        Op::Times => y_val * z_val,
+        Op::Div   => y_val / z_val,
+    };
+    state.insert(x, result);
+}
+
+fn exec_goto(n: i32) -> usize{
+    let goto_num = n as usize;
+    goto_num - 1usize
+}
+
+fn exec_if(state: &mut HashMap<String, i32>, x: String, opr: Opr, n: i32, line: usize) -> usize {
+    let x_val = *state.get(&x).unwrap();
+    let result = match opr {
+        Opr::LessThan => x_val < 0,
+        Opr::Eq       =>x_val == 0,
+    };
+    if result {
+        let goto_num = n as usize;
+        goto_num - 1usize
+    } else {
+        line
     }
 
-    
-    match y {
-        Op::Plus  => println!("Plus!"),
-        Op::Minus => println!("Minus!"),
-        Op::Times => println!("Times!"),
-        Op::Div   => println!("Div!"),
-    }
-
-    match z {
-        I::Constant(x, y)  => println!("Hello instruction!"),
-        _ => println!("Bullshit")
-    }
-
-    // let x: I = I::Oper{ Plus, 5, 10, 9 };
-    // let y: I = I::IfSt{ 10, Opr::LessThan, 5 };
-    // println!("Hello, world!");
-    // println!("{}", x);
-    // println!("{}", y);
-
-    */
+}
